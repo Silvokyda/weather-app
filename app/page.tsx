dfd7837a59c87ef3c06e6e145a26fe19af8566d5
Silvanus Oketch <deseve48@gmail.com>
2023-09-19 09:38:28 +0300

    initial home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-// import Image from 'next/image'
+"use client";
+import React from 'react';
+import Input from "./component/Input"
 
-export default function Home() {
+const Home = () => {
   return (
-    <h1>Hello world</h1>
+    <div className="bg-cover bg-gradient-to-r from-blue-500 to to-blue-300 h-screen">
+      <div className="bg-white/25 w-full flex flex-col h-full">
+        {/* INPUT AND LOGO */}
+        <div className="flex flex-col md:flex-row justify-between items-center p-12">
+          <Input />
+          <h1 className='mb-8 md:mb-0 order-1 text-white py-2 px-4 rounded-xl font-bold'>Weather App</h1>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default Home;
