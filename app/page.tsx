8f340840e2af4c1a6839d85622f0d38084e92f0f
Silvanus Oketch <deseve48@gmail.com>
2023-09-21 14:34:49 +0300

    types

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,21 @@ import React from 'react';
 import Input from "./component/Input"
 import { useState } from 'react';
 
-const Home = () => {
-  interface WeatherData {
-    current: {
-      temp_f: number;
-      // Add other properties as needed
-    };
+interface WeatherData {
+  current: {
+    temp_f: number;
     // Add other properties as needed
-  }
-  const [data, setData] = useState<WeatherData | {}>({});
-  const [location, setLocation] = useState("")  
-  const [error, setError] = useState("")
+  };
+  // Add other properties as needed
+}
 
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=908a5d812821428180293330232109&q=${location}&days=7&aqi=yes&alerts=yes`
-  
+const Home = () => {
+  const [data, setData] = useState<WeatherData | {}>({});
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=908a5d812821428180293330232109&q=${location}&days=7&aqi=yes&alerts=yes`;
+
   const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       e.preventDefault();
@@ -34,17 +35,17 @@ const Home = () => {
         setData({});
       }
     }
-  };  
+  };
 
   return (
     <div className="bg-cover bg-gradient-to-r from-blue-500 to to-blue-300 h-screen">
       <div className="bg-white/25 w-full flex flex-col h-fit">
         {/* INPUT AND LOGO */}
         <div className="flex flex-col md:flex-row justify-between items-center p-12">
-          <Input handleSearch={handleSearch} setLocation={setLocation}/>
+          <Input handleSearch={handleSearch} setLocation={setLocation} />
           <h1 className='mb-8 md:mb-0 order-1 text-white py-2 px-4 rounded-xl italic font-bold'>Weather App</h1>
         </div>
-        {data.current ? <div>{data.current.temp_f}</div>: null}
+        {data && 'current' in data ? <div>{data.current.temp_f}</div> : null}
       </div>
     </div>
   )
