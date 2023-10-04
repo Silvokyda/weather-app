c36c0393c44913ae58b1f12049e53a729b0fbf73
Silvanus Oketch <97881965+Silvokyda@users.noreply.github.com>
2023-10-05 00:14:03 +0300

    weather app to WeatherHub

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ const Home = () => {
   {
     content = (
       <div className="text-white text-center h-screen mt-[5rem]">
-        <h2 className='text-3xl font-bold mb-4'>Welcome to the weather app</h2>
+        <h2 className='text-3xl font-bold mb-4'>Welcome to the WeatherHub</h2>
         <p className="text-xl">Enter a city name to get the weather forecast</p>
       </div>
     )
